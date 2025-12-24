cb649cad0b96e2c30083301f25e5da83df226d65
Tommy <tommyrdwilson@protonmail.com>
2025-12-24 22:26:24 +0000

    rewrite in svelte 5

---
+++ src/routes/+page.svelte
@@ -0,0 +1,79 @@
+<script>
+    import { game } from "./game.svelte";
+
+    let guessFest = new game;
+
+
+</script>
+
+
+<div class="app" class:anim-flash-red={guessFest.anim.isFlashRed} class:anim-flash-green={guessFest.anim.isFlashGreen}>
+    <div class="container">
+
+        {#if guessFest.state.isMainMenu && !guessFest.state.isRunning}
+            <div class="main-title">
+                <h1>GuessFest</h1>
+            </div>
+
+            <div class="game-content">
+
+            </div>
+            
+            <div class="play-button">
+                <button onclick={() => guessFest.startGame()}>
+                    <p>PLAY</p>
+                </button>
+            </div>
+
+        {:else if guessFest.state.isRunning && guessFest.timeLeft > 0}
+            <div class="main-title">
+                <div class="timer">
+                    <h1>{guessFest.timeLeft}</h1>
+                </div>
+            </div>
+
+            <div class="game-content">
+                <h1>{guessFest.activeItem}</h1>
+            </div>
+            
+            <div class="controls">
+                <button class="pass-btn" onclick={() => guessFest.passBtn()}>
+                    <p>PASS</p>
+                </button>
+                <button class="correct-btn" onclick={() => guessFest.correctBtn()}>
+                    <p>CORRECT</p>
+                </button>
+            </div>
+
+        {:else if guessFest.timeLeft <= 0}
+            <div class="main-title">
+                <h1>Game Over!</h1>
+            </div>
+
+
+            <div class="game-content">
+
+                <div class="score">
+                    <h1>You scored: {guessFest.getScore()}</h1>
+                </div>
+
+                <div class="game-overview">
+                    {#each Object.entries(guessFest.answers) as [key, val]}
+                        {#if val == true}
+                        <h2 style="color: green">{key}</h2>
+                        {:else if val == false}
+                        <h2 style="color: red">{key}</h2>
+                        {/if}    
+                    {/each}
+                </div>
+
+            </div>
+            
+            <div class="play-button">
+                <button onclick={() => guessFest.restart()}>
+                    <p>PLAY AGAIN</p>
+                </button>
+            </div>
+        {/if}
+    </div>
+</div>
---
+++ src/routes/game.svelte.js
@@ -0,0 +1,144 @@
+import items from "./items.json";
+
+export class game {
+
+    state = $state({
+        isMainMenu: true,
+        isRunning: false,
+        isGameOver: false
+    });
+
+    anim = $state({
+        isFlashRed : false,
+        isFlashGreen : false
+    })
+
+
+    timeTotal = 5;
+    timeLeft = $state(0);
+    activeTimer = null;
+    
+    gameItems = $state([]);
+    activeItem = $state("");
+
+
+    answers = $state({});
+
+
+
+    sleep = async (ms) => {
+	    return new Promise((resolve) => setTimeout(resolve, ms));
+    }
+
+    flattenDictByKeys = (obj, keysToFlatten) => {
+        return keysToFlatten.flatMap(key => {
+            if (!Array.isArray(obj[key])) {
+                throw new Error(`Expected array at key "${key}"`);
+            }
+            return obj[key];
+        });
+    }
+
+
+    shuffle = (array) => {
+        for (let i = array.length - 1; i > 0; i--) {
+            // Pick a random index from 0 to i (inclusive)
+            const j = Math.floor(Math.random() * (i + 1));
+
+            // Swap elements i and j
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+        return array;
+    }
+
+
+	startTimer() {
+		// Reset time values and clear timers:
+		this.timeLeft = this.timeTotal;
+
+		if (this.activeTimer !== null) {
+			clearInterval(this.activeTimer);
+		}
+
+		// start new countdown
+		this.activeTimer = setInterval(() => {
+			if (this.timeLeft > 0) {
+				this.timeLeft -= 1;
+			} else {
+				clearInterval(this.activeTimer);
+				this.activeTimer = null;
+                this.isGameOver = true;
+			}
+		}, 1000);
+	
+    }
+
+    correctBtn = async () => {
+        // Play flash animation
+        this.anim.isFlashGreen = true;
+        await this.sleep(100);
+        this.anim.isFlashGreen = false;
+
+        //
+        this.answers[this.activeItem] = true;
+        this.activeItem = this.gameItems.shift();
+    }
+
+    passBtn = async () => {
+        // Play flash animation
+        this.anim.isFlashRed = true;
+        await this.sleep(100);
+        this.anim.isFlashRed = false;
+
+        // 
+        this.answers[this.activeItem] = false;
+        this.activeItem = this.gameItems.shift();
+    }
+
+    getScore = () => {
+        let score = 0;
+        
+        for (const [key, val] of Object.entries(this.answers)) {
+            if (val === true) {
+                score += 1;
+            }
+        }
+
+        return score;
+    }
+
+
+    startGame = () => {
+
+        this.answers = [];
+
+        this.state.isMainMenu = false;
+
+        this.state.isRunning = true;
+
+
+        this.gameItems = this.flattenDictByKeys(items, [
+            "celebrities", "politicians", "musicians", "sports", 
+            "fictionalCharacters", "landmarks", "tvShows", "movies"
+        ]);
+
+        this.gameItems = this.shuffle(this.gameItems);
+
+        this.activeItem = this.gameItems.shift();
+
+
+        this.startTimer();
+
+    }
+
+
+    restart = () => {
+        this.state.isMainMenu = true;
+        this.state.isRunning = false;
+        this.state.isGameOver = false;
+
+        this.answers = [];
+    }
+
+
+}
